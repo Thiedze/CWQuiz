fdd3a110b2c3e2ecc14482b3f1e576a6c12fecfa
sebastian <sthiems@sieber-riedl.de>
2021-03-02 18:29:46 +0100

    Switching to Visual Studio Pro

---
+++ cwquiz-server/Startup.cs
@@ -0,0 +1,12 @@
+[assembly: FunctionsStartup(typeof(CW.Thiedze.Startup))]
+
+namespace CW.Thiedze
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+         
+        }
+    }
+}
